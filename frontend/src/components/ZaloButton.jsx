ba5beda151bef944148b507d148f6677432370b2
namth1793 <namtranhoai179.3@gmail.com>
2026-07-26 10:47:24 +0700

    update

--- frontend/src/components/ZaloButton.jsx
+++ frontend/src/components/ZaloButton.jsx
@@ -2,7 +2,7 @@ import { IconZalo } from './Icons';
 
 export default function ZaloButton() {
   return (
-    <div className="fixed bottom-6 right-6 z-40 w-14 h-14">
+    <div className="fixed bottom-24 right-6 z-40 w-14 h-14">
       <span className="absolute inset-0 rounded-full bg-[#0068FF] opacity-60 animate-ping" />
       <a
         href="https://zalo.me/84397807877"
